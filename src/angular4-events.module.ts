eaed6311d0c179f99be4a403f4fc53d07d1a6a2f
quentin <quentin.ligier@bluewin.ch>
2018-01-23 17:22:47 +0100

    Clean code

--- src/angular4-events.module.ts
+++ src/angular4-events.module.ts
@@ -2,17 +2,17 @@ import { EventsService } from './angular4-events.service';
 import { ModuleWithProviders, NgModule } from '@angular/core';
 
 @NgModule({
-  providers: [
-    EventsService
-  ]
+    providers: [
+        EventsService
+    ]
 })
 export class EventsModule {
-  public static forRoot(): ModuleWithProviders {
-    return {
-      ngModule: EventsModule,
-      providers: [
-        EventsService
-      ]
-    };
-  }
+    public static forRoot(): ModuleWithProviders {
+        return {
+            ngModule: EventsModule,
+            providers: [
+                EventsService
+            ]
+        };
+    }
 }
--- src/angular4-events.service.ts
+++ src/angular4-events.service.ts
@@ -7,52 +7,54 @@ const ServiceName: string = "Events Service";
 
 @Injectable()
 export class EventsService implements IEventsService {
-	private events = { };
+    private events = {};
 
-	constructor() { }
+    constructor() {}
 
-	public subscribe(event: string): Observable<any>;
-	public subscribe(event: string, callback: (value: any) => void): Subscription;	
-	public subscribe(event: string, callback: (value: any) => void, error: (error: any) => void): Subscription;
-	public subscribe(event: string, callback: (value: any) => void, error: (error: any) => void, complete: () => void): Subscription;
-	public subscribe(event: string, callback?: (value: any) => void, error?: (error: any) => void, complete?: () => void) {
-		if (!event) {
-			throw new Error(`[${ServiceName}] => Subscription method must get event name.`);
-		}
+    public subscribe(event: string): Observable<any>;
+    public subscribe(event: string, callback: (value: any) => void): Subscription;
+    public subscribe(event: string, callback: (value: any) => void, error: (error: any) => void): Subscription;
+    public subscribe(event: string, callback: (value: any) => void, error: (error: any) => void, complete: () => void): Subscription;
+    public subscribe(event: string, callback?: (value: any) => void, error?: (error: any) => void, complete?: () => void) {
+        if (!event) {
+            throw new Error(`[${ServiceName}] => Subscription method must get event name.`);
+        }
 
-		if (this.events[event] === undefined) {
-			this.events[event] = new Subject<any>();
-		}
+        if (this.events[event] === undefined) {
+            this.events[event] = new Subject<any>();
+        }
 
-		if (typeof callback !== 'function') {
-			return this.events[event].asObservable();
-		} else {
-			return this.events[event].asObservable().subscribe(callback, error, complete);
-		}
-	}
+        if (typeof callback !== 'function') {
+            return this.events[event].asObservable();
+        }
+        else {
+            return this.events[event].asObservable().subscribe(callback, error, complete);
+        }
+    }
 
-	public publish(event: string, eventObject?: any) {
-		if (!event) {
-			throw new Error(`[${ServiceName}] => Publish method must get event name.`);
-		} else if (!this.events[event]) {
-			return;
-		}
+    public publish(event: string, eventObject?: any): void {
+        if (!event) {
+            throw new Error(`[${ServiceName}] => Publish method must get event name.`);
+        }
+        else if (!this.events[event]) {
+            return;
+        }
 
-		this.events[event].next(eventObject);
-	}
+        this.events[event].next(eventObject);
+    }
 }
 
 export interface IEventsService {
-	publish(event: string, eventObject?: any);
-	subscribe(event: string): Observable<any>;
-	subscribe(event: string, callback: (value: any) => void): Subscription;
-	subscribe(event: string, callback: (value: any) => void, error: (error: any) => void): Subscription;
-	subscribe(event: string, callback: (value: any) => void, error: (error: any) => void, complete: () => void): Subscription;
+    publish(event: string, eventObject?: any): void;
+    subscribe(event: string): Observable<any>;
+    subscribe(event: string, callback: (value: any) => void): Subscription;
+    subscribe(event: string, callback: (value: any) => void, error: (error: any) => void): Subscription;
+    subscribe(event: string, callback: (value: any) => void, error: (error: any) => void, complete: () => void): Subscription;
 }
 
 interface ISubscribe{
-	(event: string): Observable<any>;
-	(event: string, callback: (value: any) => void): Subscription;
-	(event: string, callback: (value: any) => void, error: (error: any) => void): Subscription;
-	(event: string, callback: (value: any) => void, error: (error: any) => void, complete: () => void): Subscription;
+    (event: string): Observable<any>;
+    (event: string, callback: (value: any) => void): Subscription;
+    (event: string, callback: (value: any) => void, error: (error: any) => void): Subscription;
+    (event: string, callback: (value: any) => void, error: (error: any) => void, complete: () => void): Subscription;
 }
